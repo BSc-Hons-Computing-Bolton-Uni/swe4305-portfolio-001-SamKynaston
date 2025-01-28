9ec81bf3c38d3bd770aa22b71c00badd21806c31
samkynaston <samuelkynno@gmail.com>
2025-01-28 14:09:49 +0000

    Added more comments for Task 3

--- Logbook/src/Week1/Task3.java
+++ Logbook/src/Week1/Task3.java
@@ -14,10 +14,16 @@ public class Task3 {
         // Allow the user to input a value indicating the width of the recentangle
         int widthValue = reader.nextInt();
 
+        // Calculate the area by multiplying the heightValue with the widthValue variable.
         int area = (heightValue * widthValue);
+
+        // Calculate the perimeter by multiplying each variable by 2, and then adding them together.
         int perimeter = ((heightValue * 2) + (widthValue * 2));
 
+        // Concatenates the area variable into the outputted string
         System.out.println("The area of this rectangle is: , " + area);
+
+        // Concatenates the perimeter variable into the outputted string
         System.out.println("The perimeter of this rectangle is: " + perimeter);
     }
 }
