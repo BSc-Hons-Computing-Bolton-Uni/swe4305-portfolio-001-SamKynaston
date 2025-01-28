42df5a161cb0c7f686b61470e4ec577ab10dfe08
samkynaston <samuelkynno@gmail.com>
2025-01-28 15:08:14 +0000

    Finished Task 4, including allowing for floats

--- Logbook/src/Week1/Task4.java
+++ Logbook/src/Week1/Task4.java
@@ -1,4 +1,21 @@
 package Week1;
+import java.util.Scanner;
 
 public class Task4 {
+    // Create a new reader variable using a new instance of Scanner
+    private static final Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        // Allows the user to input a number for variable "numberOne"
+        float numberOne = reader.nextFloat();
+
+        // Allows the user to input a number for variable "numberTwo"
+        float numberTwo = reader.nextFloat();
+
+        // Calculate the Average for numberOne and numberTwo
+        float average = ((numberOne + numberTwo) / 2);
+
+        System.out.println("The Average of these two numbers is: " + average);
+    }
 }
