c31b7cff45c5f7c0773a2af21ee9ab2713fd6b97
samkynaston <samuelkynno@gmail.com>
2025-02-22 13:47:25 +0000

    added marks and courses to the main function.

--- Logbook/src/Week4/Main.java
+++ Logbook/src/Week4/Main.java
@@ -2,6 +2,7 @@ package Week4;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class Module {
     public String ModuleName;
@@ -22,12 +23,14 @@ class Student {
     public int ID;
     public String Name;
     public Course Course;
-    public Integer[] Marks = new Integer[4];
+    public List<Integer> Marks = new ArrayList<Integer>(4);
 
-    public Student(int ID, String name, Integer[] marks) {
+    public Student(int ID, String name, List<Integer> marks) {
         this.ID = ID;
         this.Name = name;
-        this.Marks = marks;
+
+        // Add the Modules that get passed into the constructor
+        this.Marks.addAll(marks);
     }
 
     public void enrol(Course course) {
@@ -51,12 +54,12 @@ class Course {
     public String Name;
     public ArrayList<Module> Modules = new ArrayList<Module>();
 
-    public Course(String course, String name, Module[] modules) {
+    public Course(String course, String name, ArrayList<Module> modules) {
         this.Course = course;
         this.Name = name;
 
         // Add the Modules that get passed into the constructor
-        this.Modules.addAll(Arrays.asList(modules));
+        this.Modules.addAll(modules);
     }
 
     public void printCourse() {
@@ -72,6 +75,37 @@ class Course {
 
 public class Main {
     public static void main(String[] args) {
+        // Module Objects
+        Module module1 = new Module("Fundamentals", "F001");
+        Module module2 = new Module("Personal Development", "PPD");
+        Module module3 = new Module("Advanced", "A001");
+        Module module4 = new Module("Proficient", "P001");
 
+        // Modules Object
+        ArrayList<Module> courseModules = new ArrayList<Module>();
+
+        courseModules.add(module1);
+        courseModules.add(module2);
+        courseModules.add(module3);
+        courseModules.add(module4);
+
+        // Course Objects
+        Course course1 = new Course("C001", "Computing", courseModules);
+        Course course2 = new Course("S001", "Science", courseModules);
+        Course course3 = new Course("H001", "History", courseModules);
+
+        // Student Objects
+        List<Integer> student0Marks = new ArrayList<Integer>(4);
+        List<Integer> student1Marks = new ArrayList<Integer>(4);
+        List<Integer> student2Marks = new ArrayList<Integer>(4);
+
+        Student student0 = new Student(100165217, "Winston Smith", student0Marks); // Create Student0
+        student0.enrol(course1); // Enrol Student0 into Course1
+
+        Student student1 = new Student(100165218, "Sam Kynaston", student1Marks);
+        student1.enrol(course1); // Enrol Student1 into Course1
+
+        Student student2 = new Student(100165219, "Melissa Anderson", student2Marks);
+        student2.enrol(course3); // Enrol Student2 into Course3
     }
 }
