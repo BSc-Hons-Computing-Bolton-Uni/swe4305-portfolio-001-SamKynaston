73c2eb2646ca1fc94ee69ed7d4fd48572a516ca6
samkynaston <samuelkynno@gmail.com>
2025-02-24 18:44:02 +0000

    removed sample data

--- Projects/ProjectA/src/Main.java
+++ Projects/ProjectA/src/Main.java
@@ -85,40 +85,6 @@ class Formatter {
 public class Main {
     // Main Function
     public static void main(String[] args) {
-        // TBD: REPLACE SAMPLE DATA WITH OWN DATA. ONLY USING FOR TESTING.
-        // Sample Artist Data
-        Artist artist1 = new Artist(1, "Anne-Marie"); // Build an artist object for Anne-Marie with ID 1
-        Artist artist2 = new Artist(2, "Ed Sheeran"); // Build an artist object for Ed Sheeran with ID 2
-        Artist artist3 = new Artist(3, "Beyonce"); // Build an artist object for Beyonce with ID 3
-        Artist artist4 = new Artist(4, "Dave"); // Build an artist object for Dave with ID 4
-        Artist artist5 = new Artist(5, "Swedish House Mafia"); // Build an artist object for SHM with ID 5
 
-        // Sample Song Data
-        Song song1 = new Song(101, "Beautiful", artist1, 863015); // Build a song object for Anne-Marie's song "Beautiful"
-        Song song2 = new Song(102, "Bad Habits", artist2, 127192542); // Build a song object for Ed Sheeran's song "Bad Habits"
-        Song song3 = new Song(103, "Halo", artist3, 991888598); // Build a song object for Beyonce's song "Halo"
-        Song song4 = new Song(104, "Clash (feat. Stormzy)", artist4, 7070513); // Build a song object for Dave's song "Clash"
-        Song song5 = new Song(105, "It Gets Better", artist5, 7563930); // Build a song object for SHM's song "It Gets Better"
-
-        User user1 = new User(1);
-        user1.AddSong(song1);
-        user1.AddSong(song3);
-        user1.AddSong(song5);
-
-        User user2 = new User(2);
-        user2.AddSong(song2);
-        user2.AddSong(song4);
-
-        user1.ShowPlaylist();
-        System.out.println();
-        user2.ShowPlaylist();
-
-        // Remove Songs
-        user1.RemoveSong(song4);
-        user1.RemoveSong(song1);
-        user1.RemoveSong(song3);
-
-        // Print User Details to test effectiveness
-        user1.ShowPlaylist();
     }
 }
