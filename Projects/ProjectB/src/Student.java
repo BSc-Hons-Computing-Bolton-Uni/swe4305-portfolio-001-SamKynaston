b19f2aa3494eed343eabe4ff62e260085368da40
samkynaston <samuelkynno@gmail.com>
2025-03-24 10:36:56 +0000

    updated class diagram for project b, class deployment

--- Projects/ProjectB/src/Student.java
+++ Projects/ProjectB/src/Student.java
@@ -1,2 +1,14 @@
 public class Student {
+    private int ID;
+    private String Forename;
+    private String Surname;
+
+    public Student() {}
+
+    public void SetID() {}
+    public void SetName() {}
+    public void AddCourse() {}
+    public void GetID() {}
+    public void GetName() {}
+    public void GetCourses() {}
 }
