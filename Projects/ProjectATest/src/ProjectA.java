f4bfeb778c18fa4d1fc5ce698c2c877d64cc4aab
samkynaston <samuelkynno@gmail.com>
2025-03-24 09:14:49 +0000

    expanded project a unit tests

--- Projects/ProjectATest/src/ProjectA.java
+++ Projects/ProjectATest/src/ProjectA.java
@@ -5,27 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ProjectA {
     // Artist Tests
     @Test
-    void createArtistAndTestID() {
+    void CreateArtistAndTestID() {
         var artist = new Artist(1, "John Smith");
         assertEquals(1, artist.GetID());
     }
 
     @Test
-    void createArtistAndTestName() {
+    void CreateArtistAndTestName() {
         var artist = new Artist(1, "John Smith");
         assertEquals("John Smith", artist.GetName());
     }
 
+    // Song Tests
     @Test
-    void createSongAndTestID() {
-        var artist = new Artist(1, "John Smith");
-        var song = new Song(1, "Test", artist, 100);
-
-        assertEquals(1, song.GetID());
-    }
-
-    @Test
-    void createSongAndTestName() {
+    void CreateSongAndTestName() {
         var artist = new Artist(1, "John Smith");
         var song = new Song(1, "Test", artist, 100);
 
@@ -33,7 +26,7 @@ public class ProjectA {
     }
 
     @Test
-    void createSongAndTestArtist() {
+    void CreateSongAndTestArtist() {
         var artist = new Artist(1, "John Smith");
         var song = new Song(1, "Test", artist, 100);
 
@@ -41,13 +34,59 @@ public class ProjectA {
     }
 
     @Test
-    void createSongAndTestPlays() {
+    void CreateSongAndTestID() {
+        var artist = new Artist(1, "John Smith");
+        var song = new Song(1, "Test", artist, 100);
+
+        assertEquals(1, song.GetID());
+    }
+
+    @Test
+    void CreateSongAndTestPlays() {
         var artist = new Artist(1, "John Smith");
         var song = new Song(1, "Test", artist, 100);
 
         assertEquals(100, song.GetPlays());
     }
 
-    // Song Tests
+    // Playlist Tests
+    @Test
+    void CreatePlaylistAndTestID() {
+        var Playlist = new Playlist(1);
+        assertEquals(1, Playlist.GetID());
+    }
 
+    @Test
+    void CreatePlaylistAndAddSongTest() {
+        var Playlist = new Playlist(1);
+        var artist = new Artist(1, "John Smith");
+        var song = new Song(1, "Test", artist, 100);
+
+        Playlist.AddSong(song);
+
+        assertTrue(Playlist.IsSongInPlaylist(song));
+    }
+
+    @Test
+    void CreatePlaylistAndRemoveSongTest() {
+        var Playlist = new Playlist(1);
+        var artist = new Artist(1, "John Smith");
+        var song = new Song(1, "Test", artist, 100);
+
+        Playlist.AddSong(song);
+        Playlist.RemoveSong(song);
+
+        assertFalse(Playlist.IsSongInPlaylist(song));
+    }
+
+    @Test
+    void CreatePlaylistAndTestSize() {
+        var Playlist = new Playlist(1);
+        var artist = new Artist(1, "John Smith");
+        var song = new Song(1, "Test", artist, 100);
+
+        Playlist.AddSong(song);
+
+        assertEquals(1, Playlist.GetSize());
+    }
 }
