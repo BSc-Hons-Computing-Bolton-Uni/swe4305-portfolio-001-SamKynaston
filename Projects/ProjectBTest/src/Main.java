a3ca722342a02fad9f30eb373b4415a087f2b6de
samkynaston <samuelkynno@gmail.com>
2025-03-31 09:26:41 +0100

    project b test class

---
+++ Projects/ProjectBTest/src/Main.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Main {
+    @Test
+    void CreateStudentAndTestID() {
+        Student student = new Student(1);
+        assertEquals(1, student.GetID());
+    }
+
+    @Test
+    void CreateStudentAndTestIDOverwrite() {
+        Student student = new Student(1);
+
+        student.SetID(2);
+
+        assertEquals(1, student.GetID());
+    }
+
+    @Test
+    void CreateStudentAndTestForename() {
+        Student student = new Student(1);
+        student.SetForename("John");
+
+        assertEquals("John", student.GetForename());
+    }
+
+    @Test
+    void CreateStudentAndTestSurname() {
+        Student student = new Student(1);
+        student.SetSurname("Smith");
+
+        assertEquals("Smith", student.GetSurname());
+    }
+
+    @Test
+    void CreateStudentAndCourseAndTestAddition() {
+        Student student = new Student(1);
+        Course course = new Course("C001");
+
+        assertTrue(student.AddCourse(course));
+    }
+
+    @Test
+    void CreateStudentAndCourseAndTestItContains() {
+        Student student = new Student(1);
+        Course course = new Course("C001");
+
+        assertTrue(student.AddCourse(course));
+        assertTrue(student.GetCourses().contains(course));
+    }
+
+    @Test
+    void CreateStudentAndCourseAndTestRemoval() {
+        Student student = new Student(1);
+        Course course = new Course("C001");
+
+        student.AddCourse(course);
+
+        assertTrue(student.RemoveCourse(course));
+    }
+}
