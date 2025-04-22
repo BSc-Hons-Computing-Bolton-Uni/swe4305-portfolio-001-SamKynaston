18a90e8e1a7839e00251f105f9f4d18948273b57
samkynaston <samuelkynno@gmail.com>
2025-04-22 14:29:13 +0100

    code cleanup

--- Projects/ProjectB/src/core/Main.java
+++ Projects/ProjectB/src/core/Main.java
@@ -56,43 +56,5 @@ public class Main {
 
     public static void main(String[] args) {
         DeploySampleData();
-
-        /*
-        // Manage objects.Student menus.Menu Option
-        MenuOption ManageStudents = new MenuOption("Manage Students");
-        ManageStudents.SetAction(() -> {
-
-            // menus.Menu for objects.Student Management
-            Menu ManageStudentsMenu = new Menu(reader);
-
-            for (Student student : Students) {
-                MenuOption StudentOption = new MenuOption(student.GetForename() + " " + student.GetSurname());
-
-                StudentOption.SetAction(() -> {});
-
-                ManageStudentsMenu.AddOption(StudentOption);
-            }
-
-            // menus.Menu Options
-            MenuOption RemoveStudent = new MenuOption("Remove objects.Student");
-        });
-
-        // Manage objects.Course menus.Menu Option
-        MenuOption ManageCourse = new MenuOption("Manage Courses");
-        ManageCourse.SetAction(() -> {
-            Menu ManageStudentsMenu = new Menu(reader);
-
-        });
-
-        // Manage objects.Module menus.Menu Option
-        MenuOption ManageModules = new MenuOption("Manage Modules");
-        ManageModules.SetAction(() -> {
-            Menu ManageStudentsMenu = new Menu(reader);
-
-        });
-
-        BaseMenu.AddOption(ManageStudents);
-        BaseMenu.AddOption(ManageCourse);
-        BaseMenu.AddOption(ManageModules);*/
     }
 }
