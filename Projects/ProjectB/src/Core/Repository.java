57200b63912794854bb1bf4cba9fdcf1e12d084e
samkynaston <samuelkynno@gmail.com>
2025-04-28 09:03:25 +0100

    added errors when adding entities that already exist to modules

--- Projects/ProjectB/src/Core/Repository.java
+++ Projects/ProjectB/src/Core/Repository.java
@@ -13,37 +13,37 @@ public class Repository {
     private static final ArrayList<Course> Courses = new ArrayList<>();
 
     public static void AddStudent(Student student) {
-        if (Students.contains(student)) { return; }
+        if (Students.contains(student)) { System.out.println("Student already exists!"); return; }
 
         Students.add(student);
     }
 
     public static void AddModule(Module module) {
-        if (Modules.contains(module)) { return; }
+        if (Modules.contains(module)) { System.out.println("Module already exists!"); return; }
 
         Modules.add(module);
     }
 
     public static void AddCourse(Course course) {
-        if (Courses.contains(course)) { return; }
+        if (Courses.contains(course)) { System.out.println("Course already exists!"); return; }
 
         Courses.add(course);
     }
 
     public static void RemoveStudent(Student student) {
-        if (!Students.contains(student)) { return; }
+        if (!Students.contains(student)) { System.out.println("Student doesn't exist!"); return; }
 
         Students.remove(student);
     }
 
     public static void RemoveModule(Module module) {
-        if (!Modules.contains(module)) { return; }
+        if (!Modules.contains(module)) { System.out.println("Module doesn't exist!"); return; }
 
         Modules.remove(module);
     }
 
     public static void RemoveCourse(Course course) {
-        if (!Courses.contains(course)) { return; }
+        if (!Courses.contains(course)) { System.out.println("Course doesn't exist!"); return; }
 
         Courses.remove(course);
     }
